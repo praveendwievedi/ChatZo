d9bbb1794c54daee8e4b482f3233d7ff4c1ceab6
praveendwivedi <praveen240903@gmail.com>
2024-10-20 06:44:12 +0530

    removing uneccessary parts

--- server/index.js
+++ server/index.js
@@ -34,11 +34,11 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-cloudinary.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET
-})
+// cloudinary.config({
+//     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key:process.env.CLOUDINARY_API_KEY,
+//     api_secret:process.env.CLOUDINARY_API_SECRET
+// })
 
 
 app.get('/test',(req,res)=>{
